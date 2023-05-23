71f9718c0a03901cb47b4297fdb852741afba7a8
gabrin03 <113719520+gabrin03@users.noreply.github.com>
2023-05-23 13:58:45 +0200

    Add files via upload

---
+++ pipex/include/pipex.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: grinella <grinella@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/08 14:16:30 by grinella          #+#    #+#             */
+/*   Updated: 2023/05/08 16:11:56 by grinella         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <unistd.h>
+# include <fcntl.h>
+
+# define ERR_PREFIX "Error: "
+# define ERR_USAGE ERR_PREFIX "usage: ./pipex file1 cmd1 cmd2 file2\n"
+
+#endif
